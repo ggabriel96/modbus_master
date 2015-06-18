3a16ffed194457915f5bb5f220f2d35c0e92276b
João Pedro Winckler Bernardi <jpwbernardi@hotmail.com>
2015-06-17 22:48:27 -0300

    Created CustomJPanel to replace button (Rumors that will work as we want :P)

---
+++ Interface/CustomJPanel.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+
+public class CustomJPanel extends JPanel implements MouseListener{
+
+    private Image backgroundImage;
+    private GUI pFrame;
+
+    public CustomJPanel(String fileName, GUI principalFrame){
+
+        pFrame = principalFrame;
+
+        try{
+            backgroundImage = ImageIO.read(new File(fileName));
+        }catch(Exception e){
+            System.out.println("Erro ao carregar imagem!");
+            System.exit(-1);
+        }
+        addMouseListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw the background image.
+        g.drawImage(backgroundImage, 0, 0, this);
+    }
+
+    public void eventOutput(String eventDescription, MouseEvent e) {
+        System.out.println(eventDescription + " detected on " + e.getComponent().getClass().getName() + ".");
+        System.out.println("X: " + e.getX() + "      Y: " + e.getY());
+    }
+
+    public void mousePressed(MouseEvent e) {
+        eventOutput("Mouse pressed (# of clicks: "
+                + e.getClickCount() + ")", e);
+
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        eventOutput("Mouse released (# of clicks: "
+                + e.getClickCount() + ")", e);
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        eventOutput("Mouse entered", e);
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+        eventOutput("Mouse exited", e);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        eventOutput("Mouse clicked (# of clicks: "
+                + e.getClickCount() + ")", e);
+
+                if(e.getX() >= 240 && e.getX() <= 860 && e.getY() >= 13 && e.getY() <= 408){
+                    pFrame.poolSelected();
+                    System.out.println("Oioioizinho");
+                }
+    }
+
+}
